eba04b609908deb0bc59a28f7c4d78d85c60792f
Rob Blanckaert <rob@basicer.com>
2020-11-25 15:02:42 -0800

    Initial commit

---
+++ scripts/index.js
@@ -0,0 +1,20 @@
+class TIsForTarget {
+
+  static ready() {
+    $(document).keydown(function(e) {
+      if (e.which == 84) {
+        if (TIsForTarget.hovering) {
+          if (game.user.targets.has(TIsForTarget.hovering))
+            TIsForTarget.hovering.setTarget(false, {user: game.user, releaseOthers: !e.shiftKey});
+          else
+            TIsForTarget.hovering.setTarget(game.user, {releaseOthers: !e.shiftKey});
+        }
+      }
+    });
+  }
+
+  static onHoverToken(token, hovered) {
+    TIsForTarget.hovering = hovered ? token : undefined;
+  }
+
+}
