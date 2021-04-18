c981c94bb055059a859d41fa2769428acf3c8cbe
Drental <74384067+Drental@users.noreply.github.com>
2021-04-18 06:26:42 +0200

    change the function

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,24 @@
-class TIsForTarget {
+class FIsForFlatFooted {
 
   static onKeyDown(e) {
-      if (e.which == 84) {
-        if (TIsForTarget.hovering) {
-          if (game.user.targets.has(TIsForTarget.hovering))
-            TIsForTarget.hovering.setTarget(false, {user: game.user, releaseOthers: !e.shiftKey});
-          else
-            TIsForTarget.hovering.setTarget(game.user, {releaseOthers: !e.shiftKey});
+      if (e.which == 70) {
+        if (FIsForFlatFooted.hovering) {
+          game.pf2e.StatusEffects.setStatus(token, [{ name: 'flatFooted', toggle: true }]);
         }
       }
   }
 
   static ready() {
-    $(document).unbind('keydown', TIsForTarget.onKeyDown);
-    $(document).keydown(TIsForTarget.onKeyDown);
+    $(document).unbind('keydown', FIsForFlatFooted.onKeyDown);
+    $(document).keydown(FIsForFlatFooted.onKeyDown);
   }
 
   static onHoverToken(token, hovered) {
-    TIsForTarget.hovering = hovered ? token : undefined;
+    FIsForFlatFooted.hovering = hovered ? token : undefined;
   }
 
 }
 
-Hooks.on('canvasReady',TIsForTarget.ready);
-Hooks.on('hoverToken', TIsForTarget.onHoverToken);
+Hooks.on('canvasReady',FIsForFlatFooted.ready);
+Hooks.on('hoverToken', FIsForFlatFooted.onHoverToken);
 
